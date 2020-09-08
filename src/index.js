7e0c0eeda263c9b6a8b06be7ac10ee623442d120
daominhduc190999 <daominhduc190999@gmail.com>
2020-09-08 16:24:26 +0700

    post method

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 const path = require('path');
 const express = require('express')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const handlebars = require('express-handlebars') 
 const app = express()
 const port = 3000
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
+
 app.use(express.static(path.join(__dirname, 'public')))
 
-
-
+app.use(express.urlencoded({
+  extended:true
+}))
+app.use(express.json())
 
 //Template engine
 app.engine('.hbs', handlebars({extname: '.hbs'}));
+
 app.set('view engine', '.hbs');
+
 app.set('views', path.join(__dirname, 'resources/views'));
+
 console.log(path.join(__dirname, 'views'));
+
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -22,6 +29,16 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
   res.render('news');
 })
+app.get('/search', (req, res) => {
+ 
+  res.render('search');
+})
+app.post('/search', (req, res) => {
+
+ console.log(req.body)
+  res.send('');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
